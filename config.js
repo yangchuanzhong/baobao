6a418ad42305f5d408e07abab2b0bf80b0dd373a
yangchuanzhong <laya66515@gmail.com>
2026-06-12 00:45:54 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://mnhqoscenagwwsbzivye.supabase.co/rest/v1/",
+  url: "https://mnhqoscenagwwsbzivye.supabase.co",
   anonKey: "sb_publishable_fb1yep1X2Zub9zf-gsFTHA_tqvU-ovT",
 };
